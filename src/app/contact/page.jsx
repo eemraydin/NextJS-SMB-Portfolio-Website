9060c97b69b69b8bf1924e74a4e3604448a854f0
eemraydin <aydinemr@yahoo.com>
2025-08-13 17:27:08 -0700

    portfolio page done

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import styles from './page.module.css';
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "../components/button/Button";
 
 const Contact = () => {
   return (
-    <div className={styles.container}>Contact</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt="Contact Us"
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="Your Name" className={styles.input} />
+          <input
+            type="email"
+            placeholder="Your Email"
+            className={styles.input}
+          />
+          <textarea
+            placeholder="Your Message"
+            className={styles.textarea}
+            cols={30}
+            rows={10}
+          ></textarea>
+          <Button url="#" text="Send" />
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "../../components/button/Button";
 
-function Category() {
+const Category = ({ params }) => {
   return (
-    <div>Category</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.categoryTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.contentTitle}>Test</h1>
+          <p className={styles.desc}>
+            This is a test paragraph for the {params.category} category.
+          </p>
+          <Button url={"/"} text={"Learn More"} className={styles.button} />
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/main.jpg"
+            alt="Test Image"
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.contentTitle}>Test</h1>
+          <p className={styles.desc}>
+            This is a test paragraph for the {params.category} category.
+          </p>
+          <Button url={"/"} text={"Learn More"} className={styles.button} />
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/main.jpg"
+            alt="Test Image"
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Category
+export default Category;
